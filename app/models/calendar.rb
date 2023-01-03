c8f03730e8e6ac05a3d0d215984056ecb960e971
Coco568 <fmrgse3435@yahoo.co.jp>
2023-01-03 20:49:27 +0900

    Change:登録した日にアイコンで表示する

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,7 +2,7 @@ class Calendar < ApplicationRecord
   belongs_to :user,dependent: :destroy
   validates :date_type, presence: true
   validates :record_date, presence: true
-  enum date_type: {watering_date: 0, pesticide_date: 1, fertilizer_date: 3}
+  enum date_type: {watering_date: 0, pesticide_date: 1, fertilizer_date: 2}
 
   def start_time
     self.record_date
