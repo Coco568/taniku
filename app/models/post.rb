dc351302f4b72577b9ecac19d655206a34e6c28b
Coco568 <fmrgse3435@yahoo.co.jp>
2023-11-16 00:43:37 +0900

    Add:投稿詳細画面と投稿ボタン

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user 
   mount_uploader :image, ImageUploader
-  validates :content,  length: {maximum: 200}
+  validates :content,  length: {maximum: 100}
 def prefecture_name
   JpPrefecture::Prefecture.find(prefecture_code).name
 end
